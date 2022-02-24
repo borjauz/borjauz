c6af6b01471d6d09a816249b0a3e031565ae251f
Borja Uz <yfj31861@educastur.es>
2022-02-24 20:22:38 +0100

    2o commit

---
+++ workSpaceEnt/PracticaEntornos/src/dominioSup/dominio/subdominio/Clase2.java
@@ -0,0 +1,11 @@
+package dominioSup.dominio.subdominio;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		System.out.print("Hola a todos " + a);
+
+	}
+//IDEA tendré un 10 en  la asignatura????
+}
---
+++ workSpaceEnt/PracticaEntornos/src/dominioSup/dominio/subdominio/nombre/Clase.java
@@ -0,0 +1,10 @@
+package dominioSup.dominio.subdominio.nombre;
+
+public class Clase {
+
+	public static void main(String[] args) {
+		System.out.print("Hola a to");
+
+	}
+
+}
